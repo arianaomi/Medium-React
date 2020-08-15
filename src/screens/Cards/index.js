58eec350e6ccd3c839357072a0a6ed1c3fd3862c
Arianaomi <arianaomi.lp@gmail.com>
2020-08-15 04:36:34 -0500

    :zap: Formulario completo
    Formulario completo

---
+++ src/screens/Cards/index.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react'
+//Custom components
+import CardForm from './Components/CardForm'
+
+function Cards() {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    fetch('https://reactsessions-ac545.firebaseio.com/equipocuatro.json')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        let parsedCards = []
+        for (let key in data) {
+          console.log(key)
+          console.log(data)
+          let card = data[key]
+          console.log('card', card)
+          card['key'] = key
+          parsedCards.push(card)
+        }
+        console.log('parsed', parsedCards)
+        if (parsedCards) {
+          setCards(parsedCards)
+        }
+      })
+  }, [])
+
+  const handleForm = (title, subtitle, author, hour, content, popular, img) => {
+    fetch('https://reactsessions-ac545.firebaseio.com/equipocuatro.json', {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify({
+        title,
+        subtitle,
+        author,
+        hour,
+        content,
+        popular,
+        img,
+      }),
+    })
+      .then(response => response.json)
+      .then(({ key }) => {
+        const newUsers = [
+          ...cards,
+          { title, subtitle, author, hour, content, popular, img, key: key },
+        ]
+        setCards(newUsers)
+      })
+  }
+
+  const UICards = cards.map(
+    ({ title, subtitle, author, hour, content, popular }, index) => (
+      <li key={index}>
+        {title} {subtitle}
+      </li>
+    )
+  )
+  return (
+    <>
+      {UICards.length ? null : <p>No hay cards</p>}
+      <ul>{UICards}</ul>
+      <CardForm callback={handleForm} />
+    </>
+  )
+}
+export default Cards
