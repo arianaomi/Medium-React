8da1d198f16429cd32e7901716cf38270cf9f4c1
Karen1501 <karencsj0391@gmail.com>
2020-08-16 14:49:28 -0500

    componentCards

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
-//screens
-import Cards from "./screens/Cards";
-import CCards from "./screens/Main";
+
 //css
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Cards />
-      <CCards />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
