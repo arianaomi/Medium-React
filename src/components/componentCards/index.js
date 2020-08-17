891da44f45d63e09b16add8f0d03359989795c6c
Karen1501 <karencsj0391@gmail.com>
2020-08-17 15:26:59 -0500

    "cardLeft"

--- src/components/componentCards/index.js
+++ src/components/componentCards/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 function CardLeft() {
